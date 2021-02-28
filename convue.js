cf6c117f5a3b9d235aef241499746a20b5548851
ddiege2s <dennis.diegel@smail.inf.h-brs.de>
2021-02-28 14:32:55 +0100

    cells identifiable via rowIndex and columnIndex classes

--- convue.js
+++ convue.js
@@ -15,11 +15,21 @@ const ConvueApp = {
             this.columnCount = getComputedStyle(matrix).getPropertyValue('--column-count')
             this.rowCount =  getComputedStyle(matrix).getPropertyValue('--row-count')
             
+            let rowIndex = 0
+            let columnIndex = 0
             for (var i = 0; i < this.columnCount*this.rowCount; i++) {
-                console.log(i)
+                console.log("cell number: " + i)
                 const cellElement = document.createElement('div')
                 console.log(cellElement)
                 cellElement.classList.add('dead-cell')
+                if(columnIndex == this.columnCount - 1) {
+                    ++rowIndex
+                }
+                columnIndex = i%this.columnCount
+                console.log("row: " + rowIndex)
+                console.log("column: " + columnIndex)
+                cellElement.classList.add("rowIndex-" + rowIndex)
+                cellElement.classList.add("columnIndex-" + columnIndex)
                 matrix.appendChild(cellElement);
             }
             console.log(matrix.children)
